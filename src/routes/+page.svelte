a936fff07bf6298a85a5510c18949d5a0a59cd37
Recep <recep.yildiz@hva.nl>
2025-09-19 12:06:03 +0200

    #1 Prototype HTML profile card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,24 @@
 <script>
   let { data } = $props();
+  const member = data.member;
 </script>
 
-<h1>Hello World</h1>
+<main class="profile-card">
+    <section class="profile-info">
+      <img
+      src={`https://fdnd.directus.app/assets/${member.mugshot}`}
+      width="250"
+      alt="student-{member.name}"/>
+      <div class="text-info">
+        <h2>{member.name}</h2>
+        <p>{member.fav_emoji}</p>
+      </div>
+      <p>{member.bio}</p>
+      <a href="https://github.com/{member.github_handle}"><i class="fa-brands fa-github"></i></a>
+    </section>
+</main>
+
+
 
 
 
